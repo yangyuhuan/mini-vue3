5c6ef1ec52f962b0216e26333fb07e8828da6965
yangyuhuan <526779045@qq.com>
2021-11-10 11:23:28 +0800

    feat:增加reactive功能，实现mini-vue3

---
+++ learn-vue3-source/reactive_dep.js
@@ -0,0 +1,30 @@
+class Dep {
+    constructor(){
+        this.subscribers = new Set()
+    }
+
+    addEffect(effect){
+        this.subscribers.add(effect)
+    }
+
+    notify(){
+        this.subscribers.forEach(effect =>{
+            effect()
+        })
+    }
+}
+
+const info = { counter: 100}
+const dep = new Dep()
+function doubleCounter(){
+    console.log(info.counter*2)
+}
+function powerCounter(){
+    console.log(info.counter*info.counter)
+}
+// doubleCounter()
+// powerCounter()
+dep.addEffect(doubleCounter)
+dep.addEffect(powerCounter)
+info.counter++
+dep.notify()
