242de9007930b21dd1933d2201501f157e5166ca
杨玉环 <yangyuhuan@csgf.cn>
2021-12-23 22:53:27 +0800

    vue3-cms实现HyForm实现双向绑定的方案

--- vue3-ts-cms/src/views/main/system/user/user.vue
+++ vue3-ts-cms/src/views/main/system/user/user.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="user">
-    <hy-form v-bind="searchFormConfig" />
+    <hy-form v-bind="searchFormConfig" v-model="formData" />
     <div class="content"></div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, ref } from 'vue'
 import HyForm from '@/base-ui/form'
 
 import { searchFormConfig } from './config/search.config'
@@ -17,8 +17,16 @@ export default defineComponent({
     HyForm
   },
   setup() {
+    const formData = ref({
+      id: '',
+      name: '',
+      password: '',
+      sport: '',
+      createTime: ''
+    })
     return {
-      searchFormConfig
+      searchFormConfig,
+      formData
     }
   }
 })
