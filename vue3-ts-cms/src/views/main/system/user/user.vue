c005fff4edd284912cbd7c57f5d0406a0713b11d
杨玉环 <yangyuhuan@csgf.cn>
2021-12-24 00:51:17 +0800

    vue3-cms HyTable组件的封装,动态插槽以及作用域插槽

--- vue3-ts-cms/src/views/main/system/user/user.vue
+++ vue3-ts-cms/src/views/main/system/user/user.vue
@@ -1,13 +1,24 @@
 <template>
   <div class="user">
     <page-search :searchFormConfig="searchFormConfig" />
-    <div class="content"></div>
+
+    <div class="content">
+      <hy-table :listData="userList" :propList="propList">
+        <template #status="scope">
+          <el-button>{{ scope.row.enable ? '启用' : '禁用' }}</el-button>
+        </template>
+        <template #createAt="scope">
+          <strong>{{ scope.row.createAt }}</strong>
+        </template>
+      </hy-table>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { computed, defineComponent, ref } from 'vue'
 import PageSearch from '@/components/page-search'
+import HyTable from '@/base-ui/table'
 
 import { searchFormConfig } from './config/search.config'
 import { useStore } from '@/store'
@@ -15,7 +26,8 @@ import { useStore } from '@/store'
 export default defineComponent({
   name: 'user',
   components: {
-    PageSearch
+    PageSearch,
+    HyTable
   },
   setup() {
     const store = useStore()
@@ -28,10 +40,29 @@ export default defineComponent({
     })
     const userList = computed(() => store.state.system.userList)
     const userCount = computed(() => store.state.system.userCount)
+    const propList = [
+      { prop: 'name', label: '用户名', minWidth: '100' },
+      { prop: 'realname', label: '真实姓名', minWidth: '100' },
+      { prop: 'cellphone', label: '手机号码', minWidth: '100' },
+      { prop: 'enable', label: '状态', minWidth: '100', slotName: 'status' },
+      {
+        prop: 'createAt',
+        label: '创建时间',
+        minWidth: '250',
+        slotName: 'createAt'
+      },
+      {
+        prop: 'updateAt',
+        label: '更新时间',
+        minWidth: '250',
+        slotName: 'updateAt'
+      }
+    ]
     return {
       searchFormConfig,
       userList,
-      userCount
+      userCount,
+      propList
     }
   }
 })
