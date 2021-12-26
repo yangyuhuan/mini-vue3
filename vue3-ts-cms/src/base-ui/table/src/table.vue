b53c94a6c87ad92b9bdb0d45c90ba82723119620
杨玉环 <yangyuhuan@csgf.cn>
2021-12-27 00:59:00 +0800

    vue3-cms pagecont的配置和组件抽取,列表数据与格式优化

--- vue3-ts-cms/src/base-ui/table/src/table.vue
+++ vue3-ts-cms/src/base-ui/table/src/table.vue
@@ -1,6 +1,27 @@
 <template>
   <div class="hy-table">
+    <div class="header">
+      <slot name="header">
+        <div class="title">{{ title }}</div>
+        <div class="handler">
+          <slot name="headerHandler"></slot>
+        </div>
+      </slot>
+    </div>
     <el-table :data="listData" border style="width: 100%">
+      <el-table-column
+        v-if="showSelectColumn"
+        type="selection"
+        align="center"
+        width="60"
+      ></el-table-column>
+      <el-table-column
+        v-if="showIndexColumn"
+        type="index"
+        label="序号"
+        align="center"
+        width="80"
+      ></el-table-column>
       <template v-for="propItem in propList" :key="propItem.prop">
         <el-table-column v-bind="propItem" align="center">
           <template #default="scope">
@@ -11,6 +32,20 @@
         </el-table-column>
       </template>
     </el-table>
+    <div class="footer">
+      <slot name="footer">
+        <el-pagination
+          @size-change="handleSizeChange"
+          @current-change="handleCurrentChange"
+          :current-page="currentPage4"
+          :page-sizes="[100, 200, 300, 400]"
+          :page-size="100"
+          layout="total, sizes, prev, pager, next, jumper"
+          :total="400"
+        >
+        </el-pagination>
+      </slot>
+    </div>
   </div>
 </template>
 
@@ -18,6 +53,10 @@
 import { defineComponent } from 'vue'
 export default defineComponent({
   props: {
+    title: {
+      type: String,
+      default: ''
+    },
     listData: {
       type: Array,
       required: true
@@ -25,6 +64,14 @@ export default defineComponent({
     propList: {
       type: Array,
       required: true
+    },
+    showIndexColumn: {
+      type: Boolean,
+      required: false
+    },
+    showSelectColumn: {
+      type: Boolean,
+      required: false
     }
   },
   setup() {
@@ -33,4 +80,29 @@ export default defineComponent({
 })
 </script>
 
-<style scoped></style>
+<style scoped lang="less">
+.header {
+  display: flex;
+  height: 45px;
+  padding: 0 5px;
+  justify-content: space-between;
+  align-items: center;
+
+  .title {
+    font-size: 20px;
+    font-weight: 700;
+  }
+
+  .handler {
+    align-items: center;
+  }
+}
+
+.footer {
+  margin-top: 15px;
+
+  .el-pagination {
+    text-align: right;
+  }
+}
+</style>
---
+++ vue3-ts-cms/src/utils/date-format.ts
@@ -0,0 +1,20 @@
+import dayjs from 'dayjs'
+import utc from 'dayjs/plugin/utc'
+
+dayjs.extend(utc)
+
+const DATE_TIME_FORMAT = 'YYYY-MM-DD HH:mm:ss'
+
+export function formatUtcString(
+  uctString: string,
+  format: string = DATE_TIME_FORMAT
+) {
+  return dayjs.utc(uctString).format(format)
+}
+
+export function formatTimeStamp(
+  timeStamp: number,
+  format: string = DATE_TIME_FORMAT
+) {
+  return ''
+}
