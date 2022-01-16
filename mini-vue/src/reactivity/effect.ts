060ea35e7e132b4da2f58878c28609f0de77a754
杨玉环 <yangyuhuan@csgf.cn>
2022-01-16 23:47:07 +0800

    feat:实现effect.scheduler功能和stop功能以及onStop的回调

--- mini-vue/src/reactivity/effect.ts
+++ mini-vue/src/reactivity/effect.ts
@@ -1,12 +1,34 @@
+import { extend } from "../shared";
+
 class ReactiveEffect {
   private _fn: any;
-  constructor(fn) {
+  public scheduler: Function | undefined;
+  deps = [];
+  active = true;
+  onStop?: () => void;
+  constructor(fn: any, scheduler?: Function) {
     this._fn = fn;
+    this.scheduler = scheduler;
   }
   run() {
     activeEffect = this;
     return this._fn();
   }
+  stop() {
+    if (this.active) {
+      cleanupEffect(this);
+      if (this.onStop) {
+        this.onStop();
+      }
+      this.active = false;
+    }
+  }
+}
+
+function cleanupEffect(effect) {
+  effect.deps.forEach((dep: any) => {
+    dep.delete(effect);
+  });
 }
 
 const targetMap = new Map();
@@ -23,21 +45,35 @@ export function track(target, key) {
     dep = new Set();
     depsMap.set(key, dep);
   }
+  if (!activeEffect) return;
   dep.add(activeEffect);
+  activeEffect.deps.push(dep);
 }
 
 export function trigger(target, key) {
   let depsMap = targetMap.get(target);
   let dep = depsMap.get(key);
   for (const effect of dep) {
-    effect.run();
+    if (effect.scheduler) {
+      effect.scheduler();
+    } else {
+      effect.run();
+    }
   }
 }
 
 let activeEffect;
-export function effect(fn) {
+export function effect(fn, options: any = {}) {
   //fn
-  const _effect = new ReactiveEffect(fn);
+  const _effect = new ReactiveEffect(fn, options.scheduler);
+  //_effect.onStop = options.onStop;
+  extend(_effect, options);
   _effect.run();
-  return _effect.run.bind(_effect);
+  const runner: any = _effect.run.bind(_effect);
+  runner.effect = _effect;
+  return runner;
+}
+
+export function stop(runner) {
+  runner.effect.stop();
 }
